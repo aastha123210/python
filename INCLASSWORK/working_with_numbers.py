ae8b3e5b0e9f11416600cd7f7ab9992da03d2c39
aastha <aasthagiri1@gmail.com>
2019-09-03 22:04:07 -0500

    python

---
+++ INCLASSWORK/working_with_numbers.py
@@ -0,0 +1,21 @@
+age = 50
+
+disciples_count - 12
+
+cost_of_coffee = 5
+
+gas_price = 2.40
+
+pi = 3.141592654
+
+radius = 5
+
+circumference = 2 * pi * radius
+
+print("circumference" + circumference)
+print(circumference)
+
+x = 5+3
+y = 2-7
+z = 5/3
+
