0d8bd4d7267f9fa06270449e6f85807221ff3a79
aastha <aasthagiri1@gmail.com>
2019-09-09 01:00:41 -0500

    my commit example

--- ASSIGNMENT/homework_assignment_2/working_with_variables.py
+++ ASSIGNMENT/homework_assignment_2/working_with_variables.py
@@ -2,14 +2,11 @@
 #Student ID:0957366
 #Due Date: September 8, 2019
 #MSITM 6341
-stockSymbol = "aapl"
-price = 204.66
-change = -4.08
 
-print ("Symbol: " + stockSymbol.upper() + ", Price: " + str(price) + ", Change: " + str(change))
-print()
+Today_Price = 220.03
+Yesterday_Price = 220.61
+Symbol = "MCD"
+print(f'Symbol: {Symbol} Price: ${Today_Price} Change: ${round(Today_Price-Yesterday_Price,2)} \n \n')
+print(f'Symbol: {Symbol} Price: ${Today_Price} Change: ${round(Today_Price-Yesterday_Price,2)} \n \n')
+print(f"Symbol: {Symbol} --- Yesterday's Price: ${Yesterday_Price}")
 
-print ("Symbol: " + stockSymbol.lower() + ", Price: " + str(price) + ", Change: " + str(change))
-print()
-
-print ("Symbol: " + stockSymbol.upper() + " --- Yesterday's Price: " + str(round(price + change,2))
