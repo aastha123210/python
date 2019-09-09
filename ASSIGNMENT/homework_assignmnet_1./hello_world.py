a8c5dfeb1a9fdb91ac7ba87ae55bb4e02cf105aa
aastha <aasthagiri1@gmail.com>
2019-09-09 15:48:14 -0500

    commit chnages assignment 1

---
+++ ASSIGNMENT/homework_assignmnet_1./hello_world.py
@@ -0,0 +1,9 @@
+#Name:Aastha Giri
+#Student ID:0957366
+#Due Date: September 1, 2019
+#MSITM 6341
+
+print("Hello World!")
+print("My Name is Astha Giri \n \n")
+print("This is my first Python program! \n \n")
+print("Have a nice day.")
