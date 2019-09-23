f3da2c78d2182c1e1ed16d21b52dc9be704a3b0d
aastha <aasthagiri1@gmail.com>
2019-09-22 19:35:19 -0500

    commit assigmnet 4

---
+++ ASSIGNMENT/homework_assignmnet_4/working_with_lists_2.py
@@ -0,0 +1,9 @@
+#python program to print list of odd numbers from 3 to 50
+odd_numbers = list(range(3,51,2))
+print(odd_numbers)
+
+
+#printing the first five odd numbers in the list using a for loop, with each number printed on a single line
+x = 0
+for x in range(0,5):
+    print(odd_numbers[x])
---
+++ INCLASSWORK/practice_assignmemts.py/working_with_odd.py
@@ -0,0 +1,20 @@
+odd_numbers = list(range(3,24,1))
+print(odd_numbers)
+
+list 1 = [3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
+only_odd = [num for num in list 1 if num % 2 = = 1]
+print(only_odd)
+
+odd_numbers = list(range(3, 46, 2))
+print(odd_numbers)
+
+odd_numbers = [3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,37,39,41,43,45,47,49]
+print("First five odd numbers from the list:")
+for odd in odd_numbers [:5]
+print(odd.title())
+
+odd_numbers = list(range(3,47,2))
+print(odd_numbers)
+
+odd_numbers = list(range(3, 47, 2))
+print(odd_numbers)
