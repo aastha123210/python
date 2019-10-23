0901928aaa19e13e133c6e6ad56c8f8a51c7c063
aastha <aasthagiri1@gmail.com>
2019-10-22 23:23:40 -0500

    commit attendence 1

---
+++ ATTENDENCE/length_implmentation.py
@@ -0,0 +1,11 @@
+#Name:Aastha Giri
+#Student ID:0957366
+#Due Date: October 22, 2019
+#MSITM 6341
+
+number_item = [15,16,21,22,40,45,47,55,57,59,67,78, 88, 76, 90,99]
+total = 0
+for number in number_item:
+    total=total+1
+print("the length of number_item is",total)
+
