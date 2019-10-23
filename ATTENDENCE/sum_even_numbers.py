bb37f960e9532f7e67bd17249a9a69ec58d88151
aastha <aasthagiri1@gmail.com>
2019-10-22 23:15:00 -0500

    commit attendance

---
+++ ATTENDENCE/sum_even_numbers.py
@@ -0,0 +1,12 @@
+#Name:Aastha Giri
+#Student ID:0957366
+#Due Date: October 22, 2019
+#MSITM 6341
+
+
+number_list = [8,9,10,23,46,88,89,90]
+total =0
+for item in number_list:
+    if item%2==0:
+        total = total + item
+print(total)
