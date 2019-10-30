2c18d2947375a66c86b92f005463b36669c7d215
aastha <aasthagiri1@gmail.com>
2019-10-30 15:06:13 -0500

    commit practice exercise 2

---
+++ ATTENDENCE/negate_the_numbers.py
@@ -0,0 +1,14 @@
+#Name:Aastha Giri
+#Student ID:0957366
+#Due Date: October 29th, 2019
+#MSITM 6341
+
+#Problem of the day 2
+
+number_list = [22,23,34,-45,-56,88,99,-121,-123,213,-245,-300,598,600,-887]
+negate_list = []
+for num in number_list:
+    num= num*-1
+    negate_list.append(num)
+print(number_list)
+print(negate_list)
